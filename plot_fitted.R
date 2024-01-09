548193573f95ba2a72afa0b628b54e05194e89d7
marianmarchal <98748276+marianmarchal@users.noreply.github.com>
2024-01-09 16:03:26 +0100

    updated plot_fitted to take (optionally) 2x2x2 design

--- plot_fitted.R
+++ plot_fitted.R
@@ -1,4 +1,4 @@
-#' Plot fitted estimates of one or two discrete variables with two levels each
+#' Plot fitted estimates of one to three discrete variables with two levels each
 #'
 #' @param model Model that the estimates are taken from
 #' @param y.lab String, Title of the y-axis
@@ -11,109 +11,135 @@
 #' @param group.levels Numeric vector, Levels of this variable - optional
 #' @param group.names Character vector, Names of the levels of this variable - optional
 #' @param group.title String, Name of this variable in the plot - optional
+#' @param wrap.var String, Grouping variable - optional
+#' @param wrap.levels Numeric vector, Levels of this variable - optional
+#' @param wrap.names Character vector, Names of the levels of this variable - optional
+#' @param wrap.title String, Name of this variable in the plot - optional
 #' @param line Boolean, whether lines are present
 #' @param confidence.level Numeric, Confidence level
 #'
 #' @return Plot with fitted estimates
 #' @export
 #'
-plot_fitted <- function(model,
-                        y.lab,
-                        plot.type = "point",
-                        x.var,
-                        x.levels = c(-1, 1),
-                        x.names,
-                        x.title,
-                        group.var = NULL,
-                        group.levels = c(-1, 1),
-                        group.names,
-                        group.title,
-                        line = T,
-                        confidence.level = 0.95){
-  #set xlevels
-  if(is.null(group.var)){
-    xlevels <- list(x.levels)
-    names(xlevels) <- c(x.var)
-  } else {
-    xlevels <- list(x.levels, group.levels)
-    names(xlevels) <- c(x.var, group.var)
-  }
+
+plot_fitted <-  function(model,
+                         y.lab = "Fitted estimates",
+                         plot.type = "point",
+                         x.var,
+                         x.levels = c(-1, 1),
+                         x.names = c("Condition A", "Condition B"),
+                         x.title = "A vs. B",
+                         group.var = NULL,
+                         group.levels = c(-1, 1),
+                         group.names = c("Condition X", "Condition Y"),
+                         group.title = "X vs. Y",
+                         wrap.var = NULL,
+                         wrap.levels = c(-1,1),
+                         wrap.names = c("Condition K", "Condition L"),
+                         wrap.title = "K vs. L",
+                         line = T,
+                         confidence.level = 0.95){
+  
+  xlevels <- list(x.levels, group.levels, wrap.levels)
+  names(xlevels) <- c(x.var, group.var, wrap.var)
   
   #set position_dodge
   pd_size <- ifelse(plot.type == "point", .2, .9)
-  pd <- position_dodge(width = pd_size)
+  pd <- ggplot2::position_dodge(width = pd_size)
+  
+  #extract estimates
+  if(is.null(wrap.var)){
+    est.dat <- effects::Effect(c(x.var, group.var),
+                               model, se = T, confidence.level = confidence.level,
+                               xlevels = xlevels) %>%
+      as.data.frame() %>%
+      dplyr::mutate({{x.title}} := ifelse(.[[1]] == x.levels[1], x.names[1], x.names[2]),
+                    {{group.title}} := ifelse(.[[2]] == group.levels[1], group.names[1], group.names[2]))
+  } else if(is.null(group.var)){
+    est.dat <- effects::Effect(x.var,
+                               model, se = T, confidence.level = confidence.level,
+                               xlevels = xlevels) %>%
+      as.data.frame() %>%
+      dplyr::mutate({{x.title}} := ifelse(.[[1]] == x.levels[1], x.names[1], x.names[2]))
+  } else {
+    est.dat <- effects::Effect(c(x.var, group.var, wrap.var),
+                               model, se = T, confidence.level = confidence.level,
+                               xlevels = xlevels) %>%
+      as.data.frame() %>%
+      dplyr::mutate({{x.title}} := ifelse(.[[1]] == x.levels[1], x.names[1], x.names[2]),
+                    {{group.title}} := ifelse(.[[2]] == group.levels[1], group.names[1], group.names[2]),
+                    {{wrap.title}} := ifelse(.[[3]] == wrap.levels[1], wrap.names[1], wrap.names[2]))
+  }
+  
+  print(est.dat)
   
   #create basis of plot
   if(is.null(group.var)){
-    base_plot <- effects::Effect(x.var,
-                                 model, se = T, confidence.level = confidence.level,
-                                 xlevels = xlevels) %>%
-      as.data.frame %>%
-      mutate({{x.title}} := ifelse(.[[1]] == x.levels[1], x.names[1], x.names[2])) %>%
-      print %>%
-      ggplot(
-        aes(x = .data[[x.title]],
-            y = fit,
-            group = 1
-        ))
+    base_plot <- ggplot2::ggplot(est.dat,
+                                 ggplot2::aes(x = .data[[x.title]],
+                                              y = fit,
+                                              group = 1
+                                 ))
   } else {
-    base_plot <- effects::Effect(c(x.var, group.var),
-                                 model, se = T, confidence.level = confidence.level,
-                                 xlevels = xlevels) %>%
-      as.data.frame %>%
-      mutate({{x.title}} := ifelse(.[[1]] == x.levels[1], x.names[1], x.names[2]),
-             {{group.title}} := ifelse(.[[2]] == group.levels[1], group.names[1], group.names[2])) %>%
-      print %>%
-      ggplot(
-        aes(x = .data[[x.title]],
-            y = fit,
-            group = .data[[group.title]],
-            fill = .data[[group.title]]
-        ))
+    base_plot <- ggplot2::ggplot(est.dat,
+                                 ggplot2::aes(x = .data[[x.title]],
+                                              y = fit,
+                                              group = .data[[group.title]],
+                                              fill = .data[[group.title]]
+                                 ))
   }
   
   #update layout
   base_plot <- base_plot +
-    theme(text = element_text(family = "serif")) +
-    theme_bw(base_size = 22) +
-    xlab(x.title) +
-    ylab(y.lab)
-  # scale_linetype_discrete(name=group.title,
-  #                     	labels=group.names
-  # )
+    ggplot2::theme(text = element_text(family = "serif")) +
+    ggplot2::theme_bw(base_size = 22) +
+    ggplot2::xlab(x.title) +
+    ggplot2::ylab(y.lab)
   
-  if(plot.type == "point"){
+  #add points or bar  
+  if(plot.type == "point" & !is.null(group.var)){
     base_plot <- base_plot +
-      geom_point(
+      ggplot2::geom_point(
         size= 3,
         position = pd)
+  } else if(plot.type == "point" & is.null(group.var)){
+    base_plot <- base_plot +
+      ggplot2::geom_point(
+        size= 3)
   } else {
     base_plot <- base_plot +
-      geom_bar(
+      ggplot2::geom_bar(
         stat = "identity",
         color = "black",
         position = pd) +
-      scale_fill_grey(start = 0.4, end = 0.8)
+      ggplot2::scale_fill_grey(start = 0.4, end = 0.8)
   }
   
+  #add lines
   if(line == T & !is.null(group.var)){
     base_plot <- base_plot +
-      geom_line(
-        aes(linetype = .data[[group.title]]),
+      ggplot2::geom_line(
+        ggplot2::aes(linetype = .data[[group.title]]),
         size = 1,
         position = pd)
   } else if(line == T & is.null(group.var)){
     base_plot <- base_plot +
-      geom_line(
-        aes(linetype = "dotted",
-            size = 1,
-            position = pd))
+      ggplot2::geom_line(
+        linetype = "dotted",
+                     size = 1)
   }
   
+  #add facet_wrap
+  if(!is.null(wrap.var)){
+    base_plot <- base_plot +
+      ggplot2::facet_wrap(~.data[[wrap.title]])
+  }
+  
+  #add error bars
   p <- base_plot +
-    geom_errorbar(
-      aes(ymin = fit-se,
-          ymax= fit+se),
+    ggplot2::geom_errorbar(
+      ggplot2::aes(ymin = fit-se,
+                   ymax= fit+se),
       size = .5,
       width = .3,
       position = pd
